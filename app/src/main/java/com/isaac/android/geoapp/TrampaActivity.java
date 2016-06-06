43d161800b2a73b3ee87a2a8d876c6715680f607
Isaac Cantun <izakntun@gmail.com>
2016-06-06 15:17:19 -0500

    ultimas modificaciones a GeoApp

---
+++ app/src/main/java/com/isaac/android/geoapp/TrampaActivity.java
@@ -0,0 +1,13 @@
+package com.isaac.android.geoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrampaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trampa);
+    }
+}
